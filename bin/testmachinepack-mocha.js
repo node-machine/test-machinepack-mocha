1201c942cd3ef808a63436c42bce8db044d68ede
Mike McNeil <mike@balderdash.co>
2015-02-06 12:37:56 -0800

    first pass

---
+++ bin/testmachinepack-mocha.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+// Test pack in current working directory using mocha test driver.
+require('../')();
---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var RawMachinepackTestRunner = require('test-machinepack').rawTestRunner;
+
+
+//
+// TODO: call mocha programatically instead of relying on omniscient `npm test` bash script in package.json
+//
+
+// Customize generic test driver for mocha
+module.exports = function mochaDriver(pathToMachinepackDir) {
+
+  // Use cwd as our path unless overridden by the arg above
+  pathToMachinepackDir = pathToMachinepackDir || process.cwd();
+
+  RawMachinepackTestRunner(function beforeRunningAnyTests(opts, done){
+    // e.g. set mocha.opts based on generic `opts` provided
+    // TODO
+    done();
+  }, function eachMachineSuite(machineIdentity, runTests){
+    describe('`'+machineIdentity+'` machine', function (){
+      runTests(function onTest(testCase, nextTestCase){
+        var jsonInputVals;
+        try {
+          jsonInputVals = JSON.stringify(testCase.using);
+        }
+        catch (e) {
+        }
+
+        if (testCase.todo) {
+          it('should exit with `'+testCase.outcome+'`'+(_.isUndefined(jsonInputVals)?'':' given input values: `'+jsonInputVals+'`') );
+          return nextTestCase();
+        }
+
+        it('should exit with `'+testCase.outcome+'`'+ (_.isUndefined(jsonInputVals)?'':' given input values: `'+jsonInputVals+'`'), function (done){
+          return nextTestCase(done);
+        });
+      });
+    });
+  }, function afterRunningAllTests(err) {
+    // Done.
+  });
+};
