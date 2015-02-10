65f4730b2423332203f144678d481cab57b234a2
Globegitter <markus.padourek@gmail.com>
2015-02-10 15:49:20 +0000

    Each test is now running in a clean tmp dir.

--- index.js
+++ index.js
@@ -4,13 +4,18 @@
 
 var _ = require('lodash');
 var RawMachinepackTestRunner = require('test-machinepack').rawTestRunner;
+var tmp = require('tmp-sync');
+var fs = require('fs-extra');
+
+var root = process.cwd();
+var tmproot = path.join(root, 'tmp');
 
 
 // Customize generic test driver for mocha
 module.exports = function mochaDriver(pathToMachinepackDir) {
 
   // Use cwd as our path unless overridden by the arg above
-  pathToMachinepackDir = pathToMachinepackDir || process.cwd();
+  pathToMachinepackDir = pathToMachinepackDir || root;
 
   RawMachinepackTestRunner(pathToMachinepackDir,function beforeRunningAnyTests(opts, done){
     // e.g. set mocha.opts based on generic `opts` provided
@@ -18,6 +23,18 @@ module.exports = function mochaDriver(pathToMachinepackDir) {
     done();
   }, function eachMachineSuite(machineIdentity, runTests){
     describe('`'+machineIdentity+'` machine', function (){
+      var tmpdir;
+
+      before(function(){
+        tmpdir = tmp.in(tmproot);
+        process.chdir(tmpdir);
+      });
+
+      after(function(){
+        process.chdir(root);
+        fs.remove(tmproot);
+      });
+
       runTests(function onTest(testCase, nextTestCase){
         var jsonInputVals;
         try {
