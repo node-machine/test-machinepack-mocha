a98933105d4e779ad6809eb8b94c207c34c24223
Scott Gress <scottmgress@gmail.com>
2015-05-20 23:59:34 -0500

    Print nice message about missing test suites.

--- index.js
+++ index.js
@@ -54,7 +54,55 @@ module.exports = function mochaDriver(pathToMachinepackDir) {
         });
       });
     });
-  }, function afterRunningAllTests(err) {
+  }, function afterRunningAllTests(err, results) {
+      after(function(){
+        if (results.length) {showMissingSuites(results);}
+      });
     // Done.
   });
 };
+
+
+function showMissingSuites(missingSuites) {
+  // Use setTimeout so that these logs appear after the mocha output
+  setTimeout(function() {
+    // Pad a line with spaces
+    var pad = function(line, lineLength) {
+      if (line.length < lineLength) {
+        line += Array((lineLength - line.length) + 1).join(" ");
+      }
+      return line;
+    };
+    // Find the longest machine ID
+    var longestMachineId = _.reduce(missingSuites, function(memo, machineId) {return machineId.length > memo ? machineId.length : memo;}, 0);
+    // Static instructional lines
+    var instrLine1 = "The following machines are missing test suites:";
+    var instrLine2 = "Try using `mp scrub` to generate test stubs!";
+    // Determine the length of each line in the output (either using the longest machine name, or the first instruction)
+    var lineLength  = longestMachineId < instrLine1.length ? instrLine1.length : longestMachineId;
+    // Create a line of stars for the top and bottom
+    var borderLine = Array(lineLength + 5).join("*");
+    // Create a line with stars on each end and spaces in the middle
+    var spacerLine = "*" + Array(lineLength + 3).join(" ") + "*";
+    // Output the top
+    console.error(borderLine);
+    // Output a spacer line
+    console.error(spacerLine);
+    // Output instruction 1
+    console.error("* " + pad(instrLine1, lineLength) + " *");
+    // Output a spacer line
+    console.error(spacerLine);
+    // Output the names of each missing machine suite
+    _.each(missingSuites, function(machineId) {
+      console.error("* " + pad(machineId, lineLength) + " *");
+    });
+    // Output a spacer line
+    console.error(spacerLine);
+    // Output instruction 2
+    console.error("* " + pad(instrLine2, lineLength) + " *");
+    // Output a spacer line
+    console.error(spacerLine);
+    // Output the bottom
+    console.error(borderLine);
+  });
+}
